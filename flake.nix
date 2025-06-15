e263d00ef76b4d49a9a882b8edf48b7a24256194
yonikek <92832837+yonikek@users.noreply.github.com>
2025-06-15 16:18:27 +0000

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "XOR cursor effect for KDE Plasma";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
+    "x86_64-linux" "aarch64-linux"
+  ] (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in rec {
+    packages.default = pkgs.kdePackages.callPackage ./package.nix { };
+
+    devShells.default = pkgs.mkShell {
+      inputsFrom = [ packages.default ];
+    };
+  });
+}
