2a33281bc4e29edaa69f154638751276e7dcc52b
yonikek <92832837+yonikek@users.noreply.github.com>
2025-06-15 16:21:29 +0000

    Create package.nix

---
+++ package.nix
@@ -0,0 +1,32 @@
+{ lib
+, stdenv
+, cmake
+, extra-cmake-modules
+, kwin
+, wrapQtAppsHook
+, qttools
+}:
+
+stdenv.mkDerivation rec {
+  pname = "kwin-effect-xorcursor";
+  version = "1.0";
+
+  src = ./.;
+
+  nativeBuildInputs = [
+    cmake
+    extra-cmake-modules
+    wrapQtAppsHook
+  ];
+
+  buildInputs = [
+    kwin
+    qttools
+  ];
+
+  meta = with lib; {
+    description = "XOR cursor effect for KDE Plasma";
+    license = licenses.gpl2;
+    homepage = "https://github.com/yonikek/kwin-effect-xorcursor";
+  };
+}
