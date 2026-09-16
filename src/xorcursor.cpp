17e62571fabea70108d6bcf953afe81973c1c472
yonikek <aoeui@disroot.org>
2026-09-16 21:55:05 +0100

    v7

--- src/xorcursor.cpp
+++ src/xorcursor.cpp
@@ -243,7 +243,7 @@ void XorCursorEffect::paintScreen(const RenderTarget &renderTarget,
 
     const QRect deviceRect = cursorDeviceRect.toAlignedRect();
     const QSize deviceSize = deviceRect.size();
-    const Region cursorRegion(Rect(deviceRect));
+    const Region cursorRegion{deviceRect};
 
     effects->paintScreen(renderTarget, viewport, mask, cursorRegion, screen);
 
